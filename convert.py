e85fd593f9b53e6499469d120d4bfb738449895a
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-07-09 16:21:12 -0400

    Completing convert.py

--- convert.py
+++ convert.py
@@ -16,7 +16,14 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
-
+    if unit_in == "c" and unit_out == "f":
+      return ((9/5) * temp + 32.0)
+    elif unit_in == "f" and unit_out == "c":
+      return ((temp - 32.0) * (5/9))
+    elif unit_in == unit_out:
+      return temp
+    else:
+      return "Invalid unit z"
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
