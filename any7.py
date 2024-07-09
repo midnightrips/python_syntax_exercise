baeca1a22a9f1ddbe64361058a8d081d33f7deb4
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-07-09 15:53:20 -0400

    Completing any7.py

--- any7.py
+++ any7.py
@@ -2,6 +2,11 @@ def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
 
     # YOUR CODE HERE 
+    seven = 7
+    if seven in nums:
+        return True
+    else:
+        return False
 
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
